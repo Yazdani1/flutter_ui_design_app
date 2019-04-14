085a28540d57482535c520a04d991cbb6598ce17
Yazdani <yaz4dani@gmail.com>
2019-04-14 17:15:24 +0600

    second

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -48,11 +48,33 @@ class _HomeState extends State<Home> {
             margin: EdgeInsets.all(10.0),
             child: new Column(
               children: <Widget>[
-
+                allData("asset/bb.jpg", "Flutter", "We Are learning flutter", "We can do this for this project"),
+                new SizedBox(height: 25.0,),
+                allData("asset/dd.jpg", "Flutter", "We Are learning flutter", "We can do this for this project"),
+                new SizedBox(height: 25.0,),
+                allData("asset/ee.jpg", "Flutter", "We Are learning flutter", "We can do this for this project"),
+                new SizedBox(height: 25.0,),
+                allData("asset/pr.jpg", "Flutter", "We Are learning flutter", "We can do this for this project")
               ],
             ),
           ),//third container
 
+          new Container(
+            margin: EdgeInsets.all(10.0),
+            child: new Text("Most Popular Product",
+            style: TextStyle(fontSize: 16.0,color: Colors.grey.withOpacity(0.9)),
+            ),
+          ),
+          
+          new Container(
+            margin: EdgeInsets.all(10.0),
+            child: new ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+
+              ],
+            ),
+          ),//fourth container here
 
         ],
       ),
@@ -62,6 +84,53 @@ class _HomeState extends State<Home> {
 
   Widget allData(String image,String name,String title,String des){
 
+    return Container(
+      child: new Row(
+        children: <Widget>[
+
+          new Container(
+            child: new ClipRRect(
+              borderRadius: BorderRadius.circular(10.0),
+              child: new Image(
+                  image: new AssetImage(image),
+                height: 80.0,
+                width: 80.0,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          new SizedBox(width: 20.0,),
+
+          new Container(
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+
+                new Text(name,
+                style: TextStyle(fontSize: 17.0,color: Colors.deepOrange),
+                ),
+                new SizedBox(height: 5.0,),
+                new Text(title,
+                style: TextStyle(fontSize: 20.0,color: Colors.white),
+                ),
+                new SizedBox(height: 4.0,),
+                
+                new Text(des,
+                style: TextStyle(fontSize: 18.0,color: Colors.grey),
+                )
+
+              ],
+            ),
+          )
+
+        ],
+      ),
+    );
+
+  }
+
+  Widget lastData(){
+    
   }
 
 }
